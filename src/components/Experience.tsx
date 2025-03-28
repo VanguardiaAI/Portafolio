0d40613cdc6ce8297d28ae8212a150ac80f2fe81
Tu Nombre <pablo.ar.luque@gmail.com>
2025-03-28 15:11:48 -0600

    First commit

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring } from "framer-motion";
 import { Building2, Briefcase } from "lucide-react";
 import { useRef } from "react";
 import { useTheme } from "../lib/ThemeContext";
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { HeroParallax } from "./ui/hero-parallax";
 import {
   motion,
   useScroll,
